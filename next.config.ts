734297a3d49fa97fdd4e1372c6c65ba0582f09a2
xiaozhe01 <xiozhe009@gmail.com>
2026-08-05 17:37:34 +0800

    Allow live-test.stakeblogs.com as a dev origin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["live-test.stakeblogs.com"],
 };
 
 export default nextConfig;
